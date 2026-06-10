a50870c651e9c3103447a5f02fab997dd87b9d58
Bhavik R K <69967720+BhavikRK@users.noreply.github.com>
2026-06-10 18:33:32 +0530

    Update playwright-script.spec.ts

--- playwright-script.spec.ts
+++ playwright-script.spec.ts
@@ -4,6 +4,6 @@
         await page.goto("https://playwright.dev/");
         await expect(page).toHaveTitle(/Playwright/);
 
-        await page.getByRole("link", { name: "Get started11" }).click();
+        await page.getByRole("link", { name: "Get started" }).click();
         await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
     }
