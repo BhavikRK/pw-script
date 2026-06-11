782f578d525e19ada25344541ec101c4758f4a3a
Bhavik R K <69967720+BhavikRK@users.noreply.github.com>
2026-06-11 10:49:35 +0530

    Update playwright-multi-script.spec.ts

--- playwright-multi-script.spec.ts
+++ playwright-multi-script.spec.ts
@@ -4,7 +4,7 @@
  * https://playwright.dev/docs/writing-tests
  */
 
-import { test, expect } from '@playwright/test';
+const { expect, test } = require('@playwright/test');
 
 // Configure the Playwright Test timeout to 210 seconds,
 // ensuring that longer tests conclude before Checkly's browser check timeout of 240 seconds.
