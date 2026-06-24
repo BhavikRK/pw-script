12a99e5076b14673beb99c428c73e7d31b34199a
Bhavik R K <69967720+BhavikRK@users.noreply.github.com>
2026-06-24 15:15:57 +0530

    Update python-script.py

--- python-script.py
+++ python-script.py
@@ -1,66 +1,17 @@
-from playwright.sync_api import sync_playwright, expect
+import re
+from playwright.sync_api import Page, expect
 
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
 
-def run():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(
-            headless=True
-        )
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwright"))
 
-        page = browser.new_page()
+def test_get_started_link(page: Page):
+    page.goto("https://playwright.dev/")
 
-        # Open application
-        page.goto("https://www.saucedemo.com/")
+    # Click the get started link.
+    page.get_by_role("link", name="Get started").click()
 
-        # Login
-        page.fill('[data-test="username"]', "standard_user")
-        page.fill('[data-test="password"]', "secret_sauce")
-        page.click('[data-test="login-button"]')
-
-        expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
-
-        # Verify products
-        expect(page.locator(".inventory_item")).to_have_count(6)
-
-        # Add products
-        page.click('[data-test="add-to-cart-sauce-labs-backpack"]')
-        page.click('[data-test="add-to-cart-sauce-labs-bike-light"]')
-
-        # Verify cart count
-        expect(page.locator(".shopping_cart_badge")).to_have_text("2")
-
-        # Open cart
-        page.click(".shopping_cart_link")
-
-        # Checkout
-        page.click('[data-test="checkout"]')
-
-        page.fill('[data-test="firstName"]', "Bhavik")
-        page.fill('[data-test="lastName"]', "Kathrotiya")
-        page.fill('[data-test="postalCode"]', "388001")
-
-        page.click('[data-test="continue"]')
-
-        # Verify summary page
-        expect(page.locator(".summary_info")).to_be_visible()
-
-        # Complete order
-        page.click('[data-test="finish"]')
-
-        expect(page.locator(".complete-header")).to_have_text(
-            "Thank you for your order!"
-        )
-
-        # Screenshot
-        page.screenshot(
-            path="checkout-success.png",
-            full_page=True
-        )
-
-        print("Checkout completed successfully")
-
-        browser.close()
-
-
-if __name__ == "__main__":
-    run()
+    # Expects page to have a heading with the name of Installation.
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
