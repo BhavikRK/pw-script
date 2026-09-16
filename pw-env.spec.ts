b80a7c59453e883786ec37e840e4b1329d6744b7
Bhavik R K <69967720+BhavikRK@users.noreply.github.com>
2026-09-16 14:52:15 +0530

    Delete pw-env.spec.ts

--- pw-env.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-
-test('login test', async ({ page }) => {
-    await page.goto('/login'); // Uses baseURL from config
-    await page.fill('#email', process.env.USER_EMAIL!);
-    await page.fill('#password', process.env.USER_PASS!);
-    await page.click('#submit');
-});
