941bad5eb83ad3a5caa1e72c17aa00d2c34e980e
Bhavik R K <69967720+BhavikRK@users.noreply.github.com>
2026-09-22 17:18:54 +0530

    Update playwright-test-script.spec.ts

--- playwright-test-script.spec.ts
+++ playwright-test-script.spec.ts
@@ -3,6 +3,8 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
     await page.goto('https://playwright.dev/');
 
+    console.log("test has title");
+
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Playwright/);
 });
@@ -12,6 +14,8 @@ test('get started link', async ({ page }) => {
 
     // Click the get started link.
     await page.getByRole('link', { name: 'Get started' }).click();
+
+    console.log("get started link");
 
     // Expects page to have a heading with the name of Installation.
     await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
