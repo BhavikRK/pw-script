9ff557ae442623894c14b416928b5e40a7bded26
Bhavik R K <69967720+BhavikRK@users.noreply.github.com>
2026-09-25 15:25:10 +0530

    Update playwright-test-script.spec.ts

--- playwright-test-script.spec.ts
+++ playwright-test-script.spec.ts
@@ -1,12 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+    await page.goto('https://apica.io/');
 
-    console.log("test has title");
-
+    console.log("Manual Console From Script");
     // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
+    await expect(page).toHaveTitle(/Telemetry/);
 });
 
 test('get started link', async ({ page }) => {
@@ -14,8 +13,6 @@ test('get started link', async ({ page }) => {
 
     // Click the get started link.
     await page.getByRole('link', { name: 'Get started' }).click();
-
-    console.log("get started link");
 
     // Expects page to have a heading with the name of Installation.
     await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
